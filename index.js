e4f08879a4c8077baf0da393a71b9f7462341986
brandonhuang <hi@brandonhuang.io>
2015-07-02 00:53:47 +0000

    updated for heroku

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(80, function() {
-  console.log("listening on port 80");
+var port = (process.env.PORT || 5000);
+
+http.listen(port, function() {
+  console.log("listening on port", port);
 });
