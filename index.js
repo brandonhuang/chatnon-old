af9a84110c3fbb6753e62fc305f23286782a378e
brandonhuang <hi@brandonhuang.io>
2015-07-02 05:21:17 +0000

    users have message colors and added current connected users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = 0;
 
 app.use(express.static('public'));
 
@@ -10,18 +11,27 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+  // Generate custom color for new user
   var user_color = 'rgb(' + (Math.floor((256-229)*Math.random()) + 230) + ',' + 
                             (Math.floor((256-229)*Math.random()) + 230) + ',' + 
                             (Math.floor((256-229)*Math.random()) + 230) + ')';
-
-  socket.emit('color', user_color);
   console.log('a user connected with color', user_color);
 
+  // Send user their color
+  socket.emit('color', user_color);
+
+  // Update current connections
+  users++;
+  io.emit('user connect', users);
+
   socket.on('disconnect', function() {
+    users--;
+    io.emit('user disconnect', users);
     console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg) {
+    console.log(msg);
     socket.broadcast.emit('chat message', msg);
   });
 });
--- public/script.js
+++ public/script.js
@@ -3,21 +3,41 @@ $(function() {
   var userColor;
 
   $('form').submit(function() {
-    message = $('#m').val();
+    msg = {
+      text: $('#m').val(),
+      userColor: userColor
+    }
 
-    socket.emit('chat message', message);
-    $('#messages').append($('<div class="chat" style="background-color: '+ userColor +';">').text(message));
+  socket.emit('chat message', msg);
+    displayMessage(msg);
     $('#m').val('');
-    $("#messages").animate({ scrollTop: $('#messages')[0].scrollHeight}, 350);
+    $("#messages").animate({scrollTop: $('#messages')[0].scrollHeight}, 350);
     return false;
   });
 
   socket.on('chat message', function(msg){
-    $('#messages').append($('<div class="chat" style="background-color: '+ userColor +';">').text(msg));
-    $("#messages").animate({ scrollTop: $('#messages')[0].scrollHeight}, 350);
+    displayMessage(msg);
+    $("#messages").animate({scrollTop: $('#messages')[0].scrollHeight}, 350);
   });
 
   socket.on('color', function(color){
     userColor = color;
   });
+
+  socket.on('user connect', function(users) {
+    console.log(users);
+    displayUsers(users);
+  });
+
+  socket.on('user disconnect', function(users) {
+    displayUsers(users);
+  });
 });
+
+function displayMessage(msg) {
+  $('#messages').append($('<div class="chat" style="background-color: '+ msg.userColor +';">').text(msg.text));
+}
+
+function displayUsers(users) {
+  $('#users').text(users);
+}
