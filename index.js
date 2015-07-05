5b2162597139f6a006b541868d48a7afebae4946
brandonhuang <hi@brandonhuang.io>
2015-07-05 01:10:35 +0000

    logging hanshake

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected with IP:', socket.handshake.remoteAddress);
+  console.log(socket.handshake);
   var startInterval = new Date().getTime() / 1000;
   var messages = 0;
 
@@ -88,3 +88,22 @@ function generateColor() {
   var color = 'hsl('+ hue +', '+ sat +'%, '+ lum +'%)';
   return color;
 }
+
+function getClientIp(req) {
+  var ipAddress;
+  // Amazon EC2 / Heroku workaround to get real client IP
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // Ensure getting client IP address still works in
+    // development environment
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};
