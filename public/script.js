dd7948e5adf1b151af42f0662f3438655b360d5a
brandonhuang <hi@brandonhuang.io>
2015-07-07 00:32:12 +0000

    refactored clientside js

--- public/script.js
+++ public/script.js
@@ -1,45 +1,17 @@
+var socket = io();
+var userId, userColor, windowState;
+var msgRate = 0;
+var usersCon = 0;
+
 $(function() {
-  var socket = io();
-  var userId;
-  var userColor;
-  var msgRate = 0;
-  var state;
-  var usersCon = 0;
+  loadName();
+  displayPageTitle();
+  decrementMsgRate();
 
-  // Grab name from localStorage if exists
-  if (localStorage.name) {
-    var name = localStorage.getItem('name');
-    $('#tag').text(name);
-  }
-  else {
-    var name = '';
-  }
-
-  updateTitle();
-
-  $('form').submit(function() {
-    msg = {
-      text: $('#m').val(),
-      userColor: userColor,
-      name: name
-    }
-
-    if(msgRate > 3) {
-      userTimeout();
-    }
-
-  socket.emit('chat message', msg);
-  msgRate++;
-  $('#m').val('');
-  return false;
+  $('form').on('submit', function(e) {
+    processMessage();
+    e.preventDefault();
   });
-
-  // Rate Limiting
-  setInterval(function() {
-    if(msgRate > 0) {
-      msgRate--;
-    }
-  }, 3000);
 
   // Usernames
   $('#tag').on('blur', function() {
@@ -53,15 +25,8 @@ $(function() {
   });
 
   socket.on('chat message', function(msg){
-    var currentScrollBottom = $('#messages').scrollTop() + $('#messages').height();
-    var currentScrollHeight = $('#messages')[0].scrollHeight;
-
     displayMessage(msg);
-    updateTitle();
-
-    if(currentScrollBottom === currentScrollHeight) {
-      $('#messages').scrollTop($('#messages')[0].scrollHeight);
-    }
+    displayPageTitle();
   });
 
   socket.on('color', function(color){
@@ -79,78 +44,126 @@ $(function() {
   });
 
   socket.on('locations update', function(locations) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-    markers = [];
-
-    locations.forEach(function(position) {
-      marker = new google.maps.Marker({
-        position: { lat: position.latitude, lng: position.longitude },
-        map: map
-      });
-
-      markers.push(marker);
-    });
+    displayMarkers(locations);
   });
 
+  // Mobile arrow behaviour
   $('#mobile-arrow').on('click', function() {
     $("html, body").animate({scrollTop: '0px'});
   });
 
   $(window).blur(function(){
-    state = 'Inactive';
+    windowState = 'Inactive';
   });
   $(window).focus(function(){
-    state = 'Active';
-    updateTitle();
+    windowState = 'Active';
+    displayPageTitle();
+    $('#m').focus();
   });
 
   // Geolocation
   navigator.geolocation.getCurrentPosition(geoSuccess);
 
-  // Functions
-  function displayMessage(msg) {
-    var tag = '';
-    if(msg.name) {
-      tag = $('<div class="tag" style="color: '+ msg.userColor +';"></div>').text(msg.name.substr(0, 12));
-    } 
-    $('#messages').append($('<div class="message" style="background-color: '+ msg.userColor +';">').text(msg.text).prepend(tag));
-  }
-
-  function displayUsers() {
-    $('#users-online').text(usersCon);
-  }
-
-  function updateTitle() {
-    if(state === "Inactive") {
-      $('title').text('! Chatnonymous');
-    }
-    else {
-      $('title').text('Chatnonymous');
-    }
-  }
-
-  function displayUserColor() {
-    $('#user-color').css('background-color', userColor);
-  }
-
-  function geoSuccess(location) {
-    position = {
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude
-    };
-    socket.emit('position', position);
-  };
-  function userTimeout() {
-    $('#messages').append($('<div class="chat" style="background-color: white; color: #c0392b; padding: 20px 0;">').text('You have been timed out for 60 seconds'));
-    $('#m').addClass('timeout').prop('disabled', true);
-    setTimeout(function() {
-      $('#m').removeClass('timeout').prop('disabled', false);
-    }, 60000);
-
-  }
 });
+
+// Functions
+function displayMessage(msg) {
+  var currentScrollBottom = $('#messages').scrollTop() + $('#messages').height();
+  var currentScrollHeight = $('#messages')[0].scrollHeight;
+  var tag = '';
+
+  if(msg.name) {
+    tag = $('<div class="tag" style="color: '+ msg.userColor +';"></div>').text(msg.name.substr(0, 12));
+  } 
+  $('#messages').append($('<div class="message" style="background-color: '+ msg.userColor +';">').text(msg.text).prepend(tag));
+
+  if(currentScrollBottom === currentScrollHeight) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+function displayUsers() {
+  $('#users-online').text(usersCon);
+}
+
+function displayPageTitle() {
+  if(windowState === "Inactive") {
+    $('title').text('! Chatnonymous');
+  }
+  else {
+    $('title').text('Chatnonymous');
+  }
+}
+
+function displayUserColor() {
+  $('#user-color').css('background-color', userColor);
+}
+
+function displayMarkers(locations) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+
+  locations.forEach(function(position) {
+    marker = new google.maps.Marker({
+      position: { lat: position.latitude, lng: position.longitude },
+      map: map
+    });
+
+    markers.push(marker);
+  });
+}
+
+function geoSuccess(location) {
+  position = {
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude
+  };
+  socket.emit('position', position);
+};
+
+function userTimeout() {
+  $('#messages').append($('<div class="chat" style="background-color: white; color: #c0392b; padding: 20px 0;">').text('You have been timed out for 60 seconds'));
+  $('#m').addClass('timeout').prop('disabled', true);
+  setTimeout(function() {
+    $('#m').removeClass('timeout').prop('disabled', false);
+  }, 60000);
+}
+
+function loadName() {
+  if (localStorage.name) {
+    var name = localStorage.getItem('name');
+    $('#tag').text(name);
+  }
+  else {
+    var name = '';
+  }
+}
+
+function processMessage(event) {
+  var msg = {
+    text: $('#m').val(),
+    userColor: userColor,
+    name: name
+  }
+
+  if(msgRate > 3) {
+    userTimeout();
+  }
+
+  socket.emit('chat message', msg);
+  msgRate++;
+  $('#m').val('');
+}
+
+function decrementMsgRate() {
+  setInterval(function() {
+    if(msgRate > 0) {
+      msgRate--;
+    }
+  }, 3000);
+}
 
 // Google Maps Code
 var map;
